8c3721b9493e9e295aee66db4720ad0fe62dc37e
cristhiancaro <cristhian.caro7472@gmail.com>
2022-05-09 20:31:13 -0500

    carga de las 2 pimeras clases

---
+++ modulo1/clase2.js
@@ -0,0 +1,46 @@
+// Esta es la variable de la edad de anderson 
+var edadAnderson = 22
+// Para poder imprimir el valor de una variable necesitamos hacer console.log
+console. log(edadAnderson)
+
+// Tipos de Datos
+// Numerico (integer, number, int)
+var numero = 10
+var numero1 = 888888888888
+var numero3 = 3.1416
+// Texto (strng, text)
+var texto1 = 'hola como estas'
+var texto2 = 'hola mundo'
+var texto3 = '10'
+// Booleanos (bool, boolean)
+var boolean = true // verdadero 
+var boolean2= false // falso
+// Arreglos, listas (array, list)
+var lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var lista2 = ['Anderson', 'Cristipher', 'Cristhian']
+var lista3 = [true, true, false]
+var lista4 = [true, 100, 'Hola']
+var listaVacia = []
+var listaDeVariables = [numero, texto1, boolean2]
+var elemento4 = lista1[3]
+
+//constantes
+const pi = 3.1416
+pi = "Esta es mi constante pi"
+alert(pi)
+
+//Modicadores
+//Para numbers
+var numero4 = 100
+var copiaNumero4 = numero4 // Copia de una variable
+// numero4 = numero4 - 10
+numero4 = numero4 + pi // numero4 = 100 + 3.1416 =103.1416
+numero4 = numero4 * 200 + 2 + 20 / 34
+numero4 = numero4 / 2
+
+// texto 
+var hola = "hola"
+var como = "como"
+var estas = "estas"
+var saludo = `${hola} ${como} ${estas}`
+alert(saludo)
