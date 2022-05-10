2f4ab3985db5c9b6190a1875b7cee53ff785b8cb
cristhiancaro <cristhian.caro7472@gmail.com>
2022-05-10 18:31:13 -0500

    clase 4 y ejercicios

---
+++ modulo1/clase4.js
@@ -0,0 +1,13 @@
+// condicionales
+// La mamá le dará permiso a la hija para ir al cine, si ella tiene dinero
+var permisoMamá
+var dineroHija = 0
+
+if (dineroHija > 0) {
+    // Codigo que se va a ejecutar solo si la condicion de sí se cumple
+    permisoMamá = true
+} else {
+    permisoMamá = false
+}
+ 
+console.log(permisoMamá)
